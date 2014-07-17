542f6e43b156f932a31806487ba907c0f37f8749
Justin Noel <github@calendee.com>
2014-07-17 12:06:06 -0500

    feat : add sample code for picking contacts
    Signed-off-by: Justin Noel <github@calendee.com>

--- www/js/app.js
+++ www/js/app.js
@@ -5,15 +5,76 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .service("ContactsService", ['$q', function($q) {
+
+        var formatContact = function(contact) {
+
+            return {
+                "displayName"   : contact.name.formatted || contact.name.givenName + " " + contact.name.familyName || "Mystery Person",
+                "emails"        : contact.emails || [],
+                "phones"        : contact.phoneNumbers || [],
+                "photos"        : contact.photos || []
+            };
+
+        };
+
+        var pickContact = function() {
+
+            var deferred = $q.defer();
+
+            if(navigator && navigator.contacts) {
+
+                navigator.contacts.pickContact(function(contact){
+
+                    deferred.resolve( formatContact(contact) );
+                });
+
+            } else {
+                deferred.reject("Bummer.  No contacts in desktop browser");
+            }
+
+            return deferred.promise;
+        };
+
+        return {
+            pickContact : pickContact
+        };
+    }])
+
+    .controller("AppCtrl", ['$scope', 'ContactsService', function($scope, ContactsService) {
+
+        $scope.data = {
+            selectedContacts : []
+        };
+
+        $scope.pickContact = function() {
+
+            ContactsService.pickContact().then(
+                function(contact) {
+                    $scope.data.selectedContacts.push(contact);
+                    console.log("Selected contacts=");
+                    console.log($scope.data.selectedContacts);
+
+                },
+                function(failure) {
+                    console.log("Bummer.  Failed to pick a contact");
+                }
+            );
+
+        }
+
+
+    }])
